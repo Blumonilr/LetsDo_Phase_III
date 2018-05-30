a3fb9ca425200efff4b16fe3fca9d6f766260b35
Blumonilr <458727969@qq.com>
2018-05-30 23:08:58 +0800

    push tagservice

---
+++ src/main/java/YingYingMonster/LetsDo_Phase_III/serviceImpl/TagServiceImpl.java
@@ -0,0 +1,21 @@
+package YingYingMonster.LetsDo_Phase_III.serviceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import YingYingMonster.LetsDo_Phase_III.service.TagService;
+
+public class TagServiceImpl implements TagService {
+
+	@Override
+	public List<String> getProjectTags(String projectId, String publisherId) {
+		// TODO 自动生成的方法存根
+		List<String> list=new ArrayList<String>();
+		list.add("名称_牛_猪_羊_狗_猫");
+		list.add("年龄_幼年_壮年_老年");
+		list.add("肤色_黑_白_灰_黄_棕_灰_黑白_黑灰_灰白");
+		list.add("性别_公_母");
+		return list;
+	}
+
+}
