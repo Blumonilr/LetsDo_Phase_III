447bab8e6cfc2a7f6c1123c7f5112b440548123e
MIKASA-MSI\Mikasa <840326461@qq.com>
2018-06-07 20:15:05 +0800

    修改登录和上传

--- src/main/resources/static/js/login/signUp.js
+++ src/main/resources/static/js/login/signUp.js
@@ -1,24 +1,15 @@
 /**
  * register.html的逻辑
  */
-function signUp(){
+function signUp(type){
+    var nickName=$("#name").val();
+    var password=$("#password").val();
     if ("" == $("#name").val().trim()) {
         $("#alert").show();
         $("#alertText").text("昵称不得为空");
         $("#name").focus();
         return false;
-    }else if ("" == $("#username").val().trim()) {
-        $("#alert").show();
-        $("#alertText").text("用户名不得为空");
-        $("#username").focus();
-        return false;
-    }else if($("#username").val().length<3){
-		$("#alertText").text("用户名长度不足");
-        $("#alert").show();
-        $("#username").focus();
-        return false;
-    }
-	else if ("" == $("#password").val().trim()) {
+    } else if ("" == $("#password").val().trim()) {
         $("#alertText").text("密码不得为空");
         $("#alert").show();
         $("#password").focus();
@@ -35,6 +26,22 @@ function signUp(){
         return false;
     }
 	else{
-		$("#signUpForm").submit();
+        var formData = new FormData();
+        formData.append("nickName",nickName);
+        formData.append("password",password);
+        $.ajax({
+            url: "/"+type+"SignUp",
+            type: "POST",
+            data: formData,
+            dataType: "text",
+            processData: false,
+            contentType: false,
+            success: function (res) {
+                alert("请牢记:你的用户名是"+res)
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert(XMLHttpRequest + "///" + textStatus + "///" + errorThrown + "\n" + "发生了预料之外的错误，请稍后再试或联系开发人员");
+            }
+        })
 	}
 }
