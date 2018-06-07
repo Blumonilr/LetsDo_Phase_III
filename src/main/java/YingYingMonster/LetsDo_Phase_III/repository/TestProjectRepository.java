fff33e7fc29d588f4f5588aa82e3c11b6b739c70
161250103 <1767855130@qq.com>
2018-06-07 11:05:32 +0800

    modify publisher service & Project entity add TestProject entity

---
+++ src/main/java/YingYingMonster/LetsDo_Phase_III/repository/TestProjectRepository.java
@@ -0,0 +1,8 @@
+package YingYingMonster.LetsDo_Phase_III.repository;
+
+import YingYingMonster.LetsDo_Phase_III.entity.TestProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestProjectRepository extends JpaRepository<TestProject, Long> {
+
+}
--- src/main/java/YingYingMonster/LetsDo_Phase_III/service/PublisherService.java
+++ src/main/java/YingYingMonster/LetsDo_Phase_III/service/PublisherService.java
@@ -2,20 +2,26 @@ package YingYingMonster.LetsDo_Phase_III.service;
 
 import java.util.List;
 
+import YingYingMonster.LetsDo_Phase_III.entity.TestProject;
 import org.springframework.web.multipart.MultipartFile;
 
-import YingYingMonster.LetsDo_Phase_III.model.Project;
+import YingYingMonster.LetsDo_Phase_III.entity.Project;
 
 public interface PublisherService {
 
-	public Project getAProject(String publisherId,String projectId);//根据publisherId,projectId获取Project对象
+	public Project getAProject(long projectId);//根据publisherId,projectId获取Project对象
 	
 	public boolean createProject(Project project, MultipartFile dataSet);
 
+	//上传测试集
+	public TestProject addTestProject(TestProject testProject, MultipartFile multipartFile);
+
+	@Deprecated
 	public boolean validateProject(String publisherId,String projectId);//新建项目时检查项目名是否合法
 
-	public List<Project> searchProjects(String publisherId,String keyword);//根据keyword查找已有的项目，支持模糊查询
+	public List<Project> searchProjects(String keyword);//根据keyword查找已有的项目，支持模糊查询
 
+	@Deprecated
 	public List<String[]> viewPushEvents(String publisherId,String projectId);//查看某个项目的提交记录
 
 	public byte[] downloadTags(String publisherId,String projectId);//下载所有标注
