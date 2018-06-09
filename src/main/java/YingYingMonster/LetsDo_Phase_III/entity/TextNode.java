d8e8b705f6558bbb17b6398d90beb2fef5b6c9d7
Blumonilr <458727969@qq.com>
2018-06-09 16:06:56 +0800

    push

---
+++ src/main/java/YingYingMonster/LetsDo_Phase_III/entity/TextNode.java
@@ -0,0 +1,70 @@
+package YingYingMonster.LetsDo_Phase_III.entity;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@Table(name="textnodes")
+public class TextNode {
+
+    @Id
+    private String name;
+    @OneToOne
+    private TextNode father;
+    private boolean isLeaf;
+
+    @ElementCollection(targetClass = String.class,fetch = FetchType.EAGER)
+    private List<String> attributions;//format:name:value1_value2_value3...
+
+    public TextNode(String name, TextNode father, boolean isLeaf,List<String> attributions) {
+        this.name = name;
+        this.father = father;
+        this.isLeaf = isLeaf;
+        this.attributions = attributions;
+    }
+
+
+    public TextNode() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TextNode getFather() {
+        return father;
+    }
+
+    public void setFather(TextNode father) {
+        this.father = father;
+    }
+
+    public boolean isLeaf() {
+        return isLeaf;
+    }
+
+    public void setLeaf(boolean leaf) {
+        isLeaf = leaf;
+    }
+
+    public List<String> getAttributions() {
+        return attributions;
+    }
+
+    public void setAttributions(List<String> attributions) {
+        this.attributions = attributions;
+    }
+
+    public void addAttribution(String attributionName,String attribution){
+        attributions.add(attributionName+":"+attribution);
+    }
+
+    public String toString(){
+        return String.format("TextNode[name=%s , father=%s , attributions=%s]",name,father.getName(),attributions.toString());
+    }
+}
